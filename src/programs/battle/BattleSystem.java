3d947fe12bfbe7aea8f766769b2c8c430a23cf93
ogawa-ryosuke <ogawa.ryosuke@compass-japan.com>
2024-05-20 13:24:13 +0900

    BattleSystem.javaの修正

--- src/programs/battle/BattleSystem.java
+++ src/programs/battle/BattleSystem.java
@@ -1,5 +1,6 @@
 package programs.battle;
 
+import programs.data.BlackJackResult;
 import programs.data.CharacterData;
 
 /**
@@ -23,13 +24,20 @@ public class BattleSystem {
 	/**
 	 * バトルの結果に基づいてHPを更新する
 	 */
-	public void updateHP() {
-		if (player.getScore() > dealer.getScore()) {
-			int newHP = dealer.getHp() - player.getAtk();
-			dealer.setHp(newHP < 0 ? 0 : newHP);
-		} else {
-			int newHP = player.getHp() - dealer.getAtk();
-			player.setHp(newHP < 0 ? 0 : newHP);
+	public void updateHP(BlackJackResult bjresult) {
+		switch (bjresult) {
+		case VICTORY:
+			int newDealerHP = dealer.getHp() - player.getAtk();
+			dealer.setHp(newDealerHP < 0 ? 0 : newDealerHP);
+			break;
+		case DEFEAT:
+			int newPlayerHP = player.getHp() - dealer.getAtk();
+			player.setHp(newPlayerHP < 0 ? 0 : newPlayerHP);
+			break;
+		case DRAW:
+			// Do nothing
+			break;
 		}
 	}
+
 }
